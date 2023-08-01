aaf5ab380193c524a7840d5e9ba31c412afd4af5
Alpha-Anas <ma08458@st.habib.edu.pk>
2023-08-01 22:54:21 +0500

    added player movements

--- GameJam'23/Assets/scripts/playerMovement.cs
+++ GameJam'23/Assets/scripts/playerMovement.cs
@@ -7,15 +7,28 @@ public class playerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow)) {
-            Debug.Log("Left Arrow");
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-5 * Time.deltaTime, 0, 0);
         }
-        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(5 * Time.deltaTime, 0, 0);
+
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 5 * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -5 * Time.deltaTime, 0);
+        }
     }
 }
